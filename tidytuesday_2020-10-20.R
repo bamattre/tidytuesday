6f32dd775570b652811a9e999b2e66c80ef1483e
Richard Bamattre <48228545+bamattre@users.noreply.github.com>
2020-10-23 14:08:29 -0500

    Add files via upload

--- tidytuesday_2020-10-20.R
+++ tidytuesday_2020-10-20.R
@@ -3,19 +3,19 @@
 # Richard Bamattre @rbamattre
 
 # load
-library(tidytuesdayR)
+library(tidytuesdayR) # Get tidy tuesday data
 
 # wrangle
-library(tidyverse)
-library(janitor)
+library(tidyverse) # of course
+library(janitor) # clean column names and nice tables
 
 # Import fonts 
-library(extrafont)
+library(extrafont) # use fonts
 
 # viz
 #install.packages("statebins", repos = c("https://cinc.rud.is", "https://cloud.r-project.org/"))
-library(statebins)
-library(ggtext)
+library(statebins) # square arranged bins for the the U.S. states
+library(ggtext) # use Markdown in ggplot2
 
 # load data
 tuesdata <- tidytuesdayR::tt_load('2020-10-20')
@@ -25,7 +25,7 @@ tuesdata <- tidytuesdayR::tt_load('2020-10-20')
 beer <- tuesdata$beer_awards %>%
   mutate(state = toupper(state),
          medal = factor(medal, levels = c("Gold", "Silver", "Bronze"),
-                        ordered = TRUE)) %>%
+                        ordered = TRUE)) %>% # make sure these get arranged
   mutate(style = case_when(str_detect(category, "Lager|Pilsener|Pilsners|Bock") ~ "Lager/Pilsner/Bock",
                            str_detect(category, "India Pale Ale") ~ "IPA",
                            str_detect(category, "Pale Ale|Amber Ale") ~ "Pale/Amber Ale",
@@ -37,46 +37,62 @@ beer <- tuesdata$beer_awards %>%
                            str_detect(category, "Wheat Beer|Wheat Ale|Witbier") ~ "Wheat Beer",
                            str_detect(category, "Sour") ~ "Sour",
                            TRUE ~ "Something Else") %>%
+            # lots of sub-styles - group into major beer styles
            factor())
 
+# Count these major styles
 beer %>%
   count(style)
 
 # Most common unclaimed beers
+# Using this view to determine what beers are being missed
 beer %>%
   filter(style == "Something Else") %>%
   count(category, sort = TRUE) %>%
   print(n = 50)
 
-# What's missing by state?
+# Which states are just going to have a top beer
+# that's "Something else"?
+# If these are very obscure beer styles, maybe that's okay
+# not to group them
 beer %>%
   count(state, category, style) %>%
   group_by(state) %>%
-  arrange(state, desc(n)) %>%
+  arrange(state, -n) %>% # arrange by n descending
   slice(1) %>%
   arrange(category) %>%
   filter(style == "Something Else") %>%
   print(n = 50)
                        
-# top awarded beer styles by state 
+# Top awarded beer styles by state 
 top_styles <- beer %>%
   count(state, category, style) %>%
   #filter(style != "Something Else") %>%
   group_by(state) %>%
-  arrange(state, desc(n)) %>%
-  slice(1) %>%
+  arrange(state, -n) %>% # get the most awarded styles by state
+  slice(1) %>% # only the top one
   ungroup() %>%
   bind_rows(tribble(~state, ~style,
-                    "WV", "None")) %>%
+                    "WV", "None")) %>% # West Virginia isn't included, add manually
   mutate(style = fct_relevel(style, "Lager/Pilsner/Bock", "IPA", "Pale/Amber Ale", 
                              "Belgian Style", "Wheat Beer", "Sour", "Red Ale", 
                              "Brown Ale", "Porter/Stout/Rye/Barrel Aged", "Something Else", 
                              "None"))
+    # Ensure styles are arrange roughly by color (looks nice)
+
+# Turns out, no Sour or Red Ales as the top awarded beer
+
+# To create more accurate beer color scheme, borrowed colors from
+# this analysis (malt colors as defined by SRM Units):
+# https://tech.webinterpret.com/calculating-beer-color-in-js/
 
 top_styles %>%
-  ggplot(aes(state = state, fill = style)) +
-  geom_statebins(radius = grid::unit(5, "pt"), na.rm = FALSE,
-                 border_size = 2, size = 1, border_col = "#242F40") +
+  ggplot(aes(state = state, fill = style)) + # aesthetics passed on to statebins
+  geom_statebins(radius = grid::unit(5, "pt"), # more rounded bins
+                 na.rm = FALSE, # keep states with no beers
+                 border_size = 2, 
+                 size = 1, 
+                 border_col = "#242F40") +
   theme_statebins(base_family = "Bahnschrift") +
   scale_fill_manual(values = c("#F6D281", # lager
                                "#EBAA31", # IPA
@@ -98,7 +114,7 @@ top_styles %>%
                       <span style = 'color:#EBAA31'> This figure looks at the most awarded beer style by state, from 1987-2020.</span>",
                       width = 100),
   caption = "Tidy Tuesday | @rbamattre | Data Source: Great American Beer Festival") +
-  theme(plot.background = element_rect(fill = "#242F40"),
+  theme(plot.background = element_rect(fill = "#242F40"), # tweaking theme to make it look nice
         text = element_text(color = "white"),
         legend.title = element_blank(),
         legend.position = "right",
@@ -107,4 +123,4 @@ top_styles %>%
         plot.title = element_text(hjust = 0.5, size = 30, color = "#EBAA31"),
         plot.subtitle = element_textbox_simple(hjust = 0.5))
 
-ggsave(r"(C:\Users\riba001\OneDrive - Hennepin County\Learning and Experiments\Tidy Tuesday\2020-10-20 beer.png)", device = "png", dpi = 150)
+ggsave("/Tidy Tuesday/2020-10-20 beer.png", device = "png", dpi = 150)
